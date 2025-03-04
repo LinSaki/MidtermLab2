94ec349563c388e1254378acdabc158a8c5e726c
Kaitlin Saqui <saquik@email.com>
2025-03-04 14:12:20 -0500

    Initial changes by Kaitlin Saqui

---
+++ midtermlab2/src/main/java/com/sheridan/App.java
@@ -0,0 +1,9 @@
+package com.sheridan;
+// SYST38634 - Software Process Management - Midterm Lab 2 - By: Kaitlin Saqui (991723734)
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi, this is my midterm lab 2!" );
+    }
+}
